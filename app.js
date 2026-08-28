d90dbd0c485d80f76e3b26b05fbfde9e3b13c63e
syhanov <dyfa2016@mail.ru>
2026-08-28 21:22:12 +0300

    feat: выполнен 1 блок

---
+++ app.js
@@ -0,0 +1,4 @@
+console.log('M04, JavaSkript подключен');
+const catalog = document.querySelector('.catalog');
+console.log(catalog);
+console.log(game);
---
+++ games.js
@@ -0,0 +1,5 @@
+const game = [
+    {
+        title: 'Elden Rring'
+    }
+];
