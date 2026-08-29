af7ced4146a7a9d82202aa0e01776646d6834707
syhanov <dyfa2016@mail.ru>
2026-08-29 20:58:31 +0300

    feat: выполнен блок 3

--- app.js
+++ app.js
@@ -3,5 +3,30 @@ const catalog = document.querySelector('.catalog');
 console.log(catalog);
 console.log(games);
 console.log(game);
-const rating = 4.0;
-console.log(`Рейтинг: ${rating.toFixed(1)}`);
+
+
+// Ответственность вызывающего кода: в try написано какого формата строку ожидает функция.
+function formatYear(released) {
+    const pieces = released.split('-');
+    return pieces[0];
+}
+
+function formatRating(rating) {
+    const number = rating.toFixed(1);
+    return number;
+}
+
+function escapeHTML(text) {
+    let result = text
+        .replaceAll('&', '&amp;')
+        .replaceAll('<', '&lt;')
+        .replaceAll('>', '&gt;')
+        .replaceAll('"', '&quot;');
+
+    return result;
+}
+
+function test() {
+    const game = 'Elden Ring';
+    console.log(game);
+}
