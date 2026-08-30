495770de08e3b96ba1927261fac015eee9cf30ac
syhanov <dyfa2016@mail.ru>
2026-08-31 00:12:34 +0300

    feat: выполнен блок 6

--- app.js
+++ app.js
@@ -1,9 +1,4 @@
-console.log('M04, JavaSkript подключен');
-const catalog = document.querySelector('.catalog');
-console.log(catalog);
-console.log(games);
-console.log(game);
-
+const catalog = document.querySelector('.games__grid')
 
 // Ответственность вызывающего кода: в try написано какого формата строку ожидает функция.
 function formatYear(released) {
@@ -26,7 +21,49 @@ function escapeHTML(text) {
     return result;
 }
 
-function test() {
-    const game = 'Elden Ring';
-    console.log(game);
+
+function renderCard(game) {
+
+    const smallImg = game.imgSrc.replace('170.jpg', '85.jpg')
+    const largeImg = game.imgSrc.replace('170.jpg', '255.jpg')
+
+    const srcSet = `${smallImg} 85w, ${game.imgSrc} 170w, ${largeImg} 255w`
+
+    return `
+                <article class="game-card game-card--catalog">
+                    <div class="game-card__cover">
+                        <img 
+                            src="${game.imgSrc}"
+                            srcset="${srcSet}"       
+                            sizes="85px"                     
+                            alt="${game.alt}" class="game-card__image"
+                            width="600"
+                            height="900">
+                        <span class="game-card__rating-badge">${game.rating.toFixed(1)}</span>
+                    </div>
+                    <div class="game-card__content">
+                        <h3 class="game-card__title">${game.title}</h3>
+                        <div class="game-card__meta">
+                            <span class="game-card__year">${game.year}</span>
+                           <span class="game-card__genre">${game.genre[0]}</span>
+                        </div>
+                    </div>
+                </article>
+            `;
+};
+
+function renderCatalog(games) {
+    if (games.length === 0) {
+       '<p>Пока пусто</p>';
+    }
+
+    let html = '';
+
+    for (let i = 0; i < games.length; i++) {
+        html += renderCard(games[i]);
+    }
+
+    return html;
 }
+
+catalog.innerHTML = (renderCatalog(games));
